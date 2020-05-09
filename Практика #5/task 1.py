4c9778af5e4cf877426431afd0d770fadbded0ac
Max1mu5 <n123av@gmail.com>
2020-05-09 18:43:01 +0300

    первый раз сделал не то задание

--- Практика #5/task 1.py
+++ Практика #5/task 1.py
@@ -1,14 +1,24 @@
 import graphics as gr
 
-SIZE_X = 400
-SIZE_Y = 400
+SIZE_X = 800
+SIZE_Y = 800
 
 window = gr.GraphWin("Model", SIZE_X, SIZE_Y)
-circle = gr.Circle(gr.Point(200, 200), 10)
-circle.draw(window)
+rectangle = gr.Rectangle(gr.Point(0, 0), gr.Point(SIZE_X, SIZE_Y))
+rectangle.setFill('green')
+rectangle.draw(window)
 
-velocity = gr.Point(1, -2)
-acceleration = gr.Point(0, 0.1)
+sun = gr.Circle(gr.Point(400, 400), 50)
+sun.setFill('yellow')
+sun.draw(window)
+
+coords = gr.Point(400, 700)
+velocity = gr.Point(2, 0)
+acceleration = gr.Point(0, 0.01)
+
+circle = gr.Circle(coords, 10)
+circle.setFill('red')
+circle.draw(window)
 
 
 def add(point_1, point_2):
@@ -18,21 +28,48 @@ def add(point_1, point_2):
     return new_point
 
 
-def check_coords(obj: gr.Circle, velocity):
-    if obj.getCenter().getX() - 10 < 0 or obj.getCenter().getX() + 10 > SIZE_X:
+def sub (point_1, point_2):
+    new_point = gr.Point(point_1.x - point_2.x,
+                         point_1.y - point_2.y)
+
+    return new_point
+
+
+def check_coords(coords, velocity):
+    if coords.x < 0 or coords.x > SIZE_X:
         velocity.x = -velocity.x
 
-    if obj.getCenter().getY() - 10 < 0 or obj.getCenter().getY() + 10 > SIZE_Y:
+    if coords.y < 0 or coords.y > SIZE_Y:
         velocity.y = -velocity.y
+
+
+def update_coords(coords, velocity):
+    return add(coords, velocity)
 
 
 def update_velocity(velocity, acceleration):
     return add(velocity, acceleration)
 
 
+def update_acceleration(ball_coords, center_coords):
+    diff = sub(ball_coords, center_coords)
+    distance_2 = (diff.x ** 2 + diff.y ** 2) ** (3/2)
+
+    G = 2000
+
+    return gr.Point(-diff.x*G/distance_2, -diff.y*G/distance_2)
+
+acceleration = update_acceleration(coords, gr.Point(400, 400))
+coords = update_coords(coords, velocity)
+velocity = update_velocity(velocity, acceleration)
 while True:
-    circle.move(velocity.x, velocity.y)
+    x = coords.x - circle.getCenter().x
+    y = coords.y - circle.getCenter().y
+    circle.move(x, y)
+
+    acceleration = update_acceleration(coords, gr.Point(400, 400))
+    coords = update_coords(coords, velocity)
     velocity = update_velocity(velocity, acceleration)
-    check_coords(circle, velocity)
+    check_coords(coords, velocity)
 
     gr.time.sleep(0.02)
