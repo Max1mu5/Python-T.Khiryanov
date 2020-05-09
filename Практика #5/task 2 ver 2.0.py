c1fbf1a705365e5dcce2d4635f2d59bbcacae425
Max1mu5 <n123av@gmail.com>
2020-05-09 18:43:19 +0300

    Добавил оптимизацию

--- Практика #5/task 2 ver 2.0.py
+++ Практика #5/task 2 ver 2.0.py
@@ -2,14 +2,13 @@ import graphics as gr
 from math import *
 """
 Улучшенное решение второго задания из практики №5.
-Пока на каждой итерации цикла рисуется новый объект, из-за это сильно падает оптимизация.
 Тут добавлена возможность изменения стартового угла наклона, длинны веревки и коэффициента затухания.
 Код взят отсюда - https://github.com/kkmoskalenko/pendulum-test и переписан на питоне 
 """
 window = gr.GraphWin('test', 500, 500)
 
-angle = -45
-deceleration = 0.001
+angle = -30
+deceleration = 0
 length = 0.6
 g = 9.80665
 
@@ -34,19 +33,17 @@ def get_time():
     return time / 1000
 
 
-def clear():
-    rect = gr.Rectangle(gr.Point(0, 0), gr.Point(500, 500))
-    rect.setFill('white')
-    rect.draw(window)
+ball = gr.Circle(gr.Point(250 + calc_x(), length + calc_y()), 10)
+ball.setFill('RED')
+ball.draw(window)
 
 
-interval = 30
+interval = 25
 while True:
-    clear()
-    ball = gr.Circle(gr.Point(250 + calc_x(), length + calc_y()), 10)
-    ball.setFill('blue')
-    ball.draw(window)
-
     time += interval
-    gr.time.sleep(0.01)
+    x = 250 + calc_x() - ball.getCenter().x
+    y = length + calc_y() - ball.getCenter().y
+    ball.move(x, y)
+
+    gr.time.sleep(0.04)
 
