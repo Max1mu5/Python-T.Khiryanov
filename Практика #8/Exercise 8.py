114785e548388fec0a62b7822ea69c0144638ab1
Max1mu5 <n123av@gmail.com>
2020-05-22 11:37:43 +0300

    Рабочая версия, но мне не нравится реализация

--- Практика #8/Exercise 8.py
+++ Практика #8/Exercise 8.py
@@ -3,7 +3,7 @@ import time
 
 turtle.shape('turtle')
 turtle.speed(4)
-turtle.width(3)
+turtle.width(2)
 
 length = 666
 
@@ -15,19 +15,14 @@ turtle.pendown()
 
 def bunch(deep, L, first_call=True):
     cur_coords = turtle.pos()
-    if deep == 0:
-        turtle.forward(L)
-        turtle.penup()
-        turtle.setpos(cur_coords[0] + 2 * L, cur_coords[1])
-        turtle.pendown()
-        turtle.forward(L)
-        return
     if first_call:
         turtle.forward(L)
         turtle.penup()
         turtle.setpos(cur_coords[0], cur_coords[1] - 20)
         turtle.pendown()
         bunch(deep, L / 3, False)
+        return
+    if deep == 0:
         return
 
     turtle.forward(L)
@@ -36,7 +31,15 @@ def bunch(deep, L, first_call=True):
     turtle.pendown()
     bunch(deep - 1, L / 3, False)
 
+    turtle.penup()
+    turtle.setpos(cur_coords[0] + 2 * L, cur_coords[1])
+    turtle.pendown()
+    new_coords = turtle.pos()
+    turtle.forward(L)
+    turtle.penup()
+    turtle.setpos(new_coords[0], new_coords[1] - 20)
+    turtle.pendown()
+    bunch(deep - 1, L / 3, False)
 
-
-bunch(3, length)
+bunch(6, length)
 time.sleep(5)
