7c0afbde9ceab6176e16376e5856c3fff2ed4867
Max1mu5 <n123av@gmail.com>
2020-05-22 15:35:24 +0300

    Последовал методу DRY и сократил кол-во строк кода

--- Практика #8/Exercise 8.py
+++ Практика #8/Exercise 8.py
@@ -13,33 +13,34 @@ turtle.forward(length / 2)
 turtle.right(180)
 turtle.pendown()
 
+
+def move(*coords):
+    turtle.penup()
+    turtle.setpos(*coords)
+    turtle.pendown()
+
+
 def bunch(deep, L, first_call=True):
     cur_coords = turtle.pos()
+
     if first_call:
         turtle.forward(L)
-        turtle.penup()
-        turtle.setpos(cur_coords[0], cur_coords[1] - 20)
-        turtle.pendown()
+        move(cur_coords[0], cur_coords[1] - 20)
         bunch(deep, L / 3, False)
         return
+
     if deep == 0:
         return
 
     turtle.forward(L)
-    turtle.penup()
-    turtle.setpos(cur_coords[0], cur_coords[1] - 20)
-    turtle.pendown()
+    move(cur_coords[0], cur_coords[1] - 20)
     bunch(deep - 1, L / 3, False)
 
-    turtle.penup()
-    turtle.setpos(cur_coords[0] + 2 * L, cur_coords[1])
-    turtle.pendown()
+    move(cur_coords[0] + 2 * L, cur_coords[1])
     new_coords = turtle.pos()
     turtle.forward(L)
-    turtle.penup()
-    turtle.setpos(new_coords[0], new_coords[1] - 20)
-    turtle.pendown()
+    move(new_coords[0], new_coords[1] - 20)
     bunch(deep - 1, L / 3, False)
 
-bunch(6, length)
+bunch(4, length)
 time.sleep(5)
